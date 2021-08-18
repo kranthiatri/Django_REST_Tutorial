0dd468631646871bf274e7bfef421849cc10e4c4
Kranthi <knuthala@atrihub.io>
2021-08-18 15:19:45 -0700

    feat(Django Rest Tutorial) : Relationships and Hyperlinked APIs Relationships and Hyperlinks added.
    [ICTRAINING-850]

--- snippets/views.py
+++ snippets/views.py
@@ -5,6 +5,11 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -32,3 +37,20 @@ class UserDetail(generics.RetrieveAPIView):
 
 def perform_create(self, serializer):
     serializer.save(owner=self.request.user)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
